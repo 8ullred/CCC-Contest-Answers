7d38258bd7f350a1991f27e51d7f6bf0d552d69e
8ullred <83183543+8ullred@users.noreply.github.com>
2022-02-16 10:12:08 -0500

    Add files via upload

---
+++ 2022 J1.py
@@ -0,0 +1,3 @@
+regBox, smolBox = 8, 3
+numOfRegBox, numOfSmolBox = int(input()), int(input())
+print(((numOfRegBox * regBox) + (numOfSmolBox * smolBox) - 28))
---
+++ 2022 J2.py
@@ -0,0 +1,11 @@
+players = []
+scores = []
+above40 = 0
+for x in range(int(input())):
+    players.append([int(input()), int(input())])
+for x in players:
+    scores.append((5*x[0]) - (3*x[1]))
+for x in scores:
+    if x > 40:
+        above40 += 1
+print(str(len(scores)) + "+") if all(x > 40 for x in scores) else print(above40)
